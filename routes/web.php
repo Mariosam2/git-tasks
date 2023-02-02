33b8c768f34bbb5184ccbb3a257b1e248b0c177c
Mariosam2 <marco.mariosaa@gmail.com>
2023-02-02 12:05:06 +0100

    Adding routes to web.php + change default redirect

--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::middleware('admin')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::resource('/apartments', ApartmentsController::class)->name('apartments');
 });
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
