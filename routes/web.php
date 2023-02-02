01a1e61993f21a5694fdc15815777ecaadd495d8
MicheleRossetti02 <michelerossetti07@gmail.com>
2023-02-02 12:25:27 +0100

    Controller+ Crud

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ApartmentsController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('apartments', ApartmentsController::class);
 
 Route::get('/', function () {
     return view('welcome');
